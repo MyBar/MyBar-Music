f917bfa060fbecf447521a04aa2c0178192cf6ac
Mybar835 <lijingui2010@163.com>
2016-05-21 18:24:57 +0800

    Update the Main Nav Bar
    Update the Main Nav Bar

--- MyBarMusic/Classes/Main/Controller/MBMainViewController.m
+++ MyBarMusic/Classes/Main/Controller/MBMainViewController.m
@@ -16,22 +16,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = MBVC_BGCOLOR;
+    
+    [self setupNavigationBar];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupNavigationBar {
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"vc_head_bg"] forBarMetrics:UIBarMetricsDefault];
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
